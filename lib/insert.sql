67f0537c3e67796a2b65947058a4f4790c7627db
JosephShin <joespam9999@gmail.com>
2022-05-19 09:59:39 -0400

    commit tree 5cd5224b98f02072f58f78d7a06c90212236811e

---
+++ lib/insert.sql
@@ -0,0 +1,31 @@
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES 
+    ('Mr. Chocolate',8,'M','black','nice',true)
+;
+
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES 
+    ('Rowdy',2,'M','brown','mean',true)
+;
+
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES 
+    ('Tabitha',3,'F','white','nice',true)
+;
+
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES 
+    ('Sergeant Brown',5,'M','brown','mean',true)
+;
+
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES 
+    ('Melissa',1,'F','blue','nice',true)
+;
+
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES 
+    ('Grinch',12,'M','black','mean',true)
+;
+
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES 
+    ('Wendy',8,'F','red','mean',true)
+;
+
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES 
+    (NULL,20,'M','white','mean',true)
+;
